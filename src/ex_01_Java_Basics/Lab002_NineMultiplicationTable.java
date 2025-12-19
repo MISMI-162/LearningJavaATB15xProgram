04517537c48ce909aea7df6213c9dda34001e8b5
MISMI RAJASHREE SATPATHY <mismirajashree@gmail.com>
2025-12-19 08:14:09 +0530

    Lab002_Using printf

---
+++ src/ex_01_Java_Basics/Lab002_NineMultiplicationTable.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basics;
+
+public class Lab002_NineMultiplicationTable {
+    public static void main(String[] args) {
+        int i = 9;
+        System.out.printf("%d * 1 = %d\n", i,i);
+        System.out.printf("%d * 2 = %d\n", i,(i*2));
+        System.out.printf("%d * 3 = %d\n", i,(i*3));
+        System.out.printf("%d * 4 = %d\n", i,(i*4));
+        System.out.printf("%d * 5 = %d\n", i,(i*5));
+        System.out.printf("%d * 6 = %d\n", i,(i*6));
+        System.out.printf("%d * 7 = %d\n", i,(i*7));
+        System.out.printf("%d * 8 = %d\n", i,(i*8));
+        System.out.printf("%d * 9 = %d\n", i,(i*9));
+        System.out.printf("%d * 10 = %d", i,(i*10));
+    }
+}
